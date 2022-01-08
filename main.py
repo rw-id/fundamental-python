3af5c098ceaeb7ba22b756fa672d95103405ad33
riffadi <!sijiloro3>
2022-01-08 08:42:40 +0700

    sequential

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Semua sintaksis dasar bahasa pemrograman terdiri dari:
+1. Sekuensial: langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulangi langkah yang sama berkali kali sampai kondisi terpenuhi
+"""
+
+# Sekuensial
+print('Ibu berkata, "Pergi Ketoko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beli 1 botol susu, dan jika ada telor beli 6"')
+print("Maka Budi berangkat ke toko")
+print("Dan mulai berbelanja")
+
